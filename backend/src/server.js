080f4102cd35bd5cf2fcba475c9de27840797f51
Harsh Bagwan <harshbagwan11@gmail.com>
2026-07-17 12:13:49 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";// make sure to write .js extension since we are importing a local file from our codebase
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth",authRoutes);  // prefix them with /api/auth
 app.use("/api/messages",messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    });
+}
 
 app.listen(PORT,()=>console.log("Server running on port :" + PORT ));
