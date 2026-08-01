2a2582a919cc4d54f89174cd55a6463f1cb757ff
Harsh Bagwan <harshbagwan11@gmail.com>
2026-08-01 23:22:54 +0530

    message routes optimised

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -36,34 +36,47 @@ export const getMessagesByUserId = async (req, res) => {
 };
 
 export const sendMessage = async (req, res) => {
-    try{
-        const {text, image} = req.body;
-        const {id: receiverId } = req.params;
-        const senderId = req.user._id;
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-        let imageUrl;
-        if(image){
-            //upload base64 image to cloudinary
-            const uploadResponse = await cloudinary.uploader.upload(image);
-            imageUrl = uploadResponse.secure_url;
-        }
-
-        const newMessage = new Message({
-            senderId,
-            receiverId,
-            text,
-            image: imageUrl
-        });
-
-        await newMessage.save();
-
-        //todo : send message in real-time if user is online - socket.io
-
-        res.status(201).json(newMessage);
-    }catch(error){
-        console.log("Error in sendMessage controller: ", error.message);
-        res.status(500).json({error: "Internal server error"});
+    if (!text && !image) {
+      return res.status(400).json({ message: "Text or image is required." });
     }
+    if (senderId.equals(receiverId)) {
+      return res
+        .status(400)
+        .json({ message: "Cannot send messages to yourself." });
+    }
+    const receiverExists = await User.exists({ _id: receiverId });
+    if (!receiverExists) {
+      return res.status(404).json({ message: "Receiver not found." });
+    }                 
+
+    let imageUrl;
+    if (image) {
+      //upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    //todo : send message in real-time if user is online - socket.io
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 export const getChatPartners = async (req, res) => {
@@ -80,12 +93,14 @@ export const getChatPartners = async (req, res) => {
         messages.map((msg) =>
           msg.senderId.toString() === loggedInUserId.toString()
             ? msg.receiverId.toString()
-            : msg.senderId.toString()
-        )
+            : msg.senderId.toString(),
+        ),
       ),
     ];
 
-    const chatPartners = await User.find({ _id: { $in: chatPartnerIds } }).select("-password");
+    const chatPartners = await User.find({
+      _id: { $in: chatPartnerIds },
+    }).select("-password");
 
     res.status(200).json(chatPartners);
   } catch (error) {
