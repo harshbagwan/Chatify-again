b7cc811f0fbbfd29de328fb7124e444ee34d0bc2
Harsh Bagwan <harshbagwan11@gmail.com>
2026-08-01 22:54:48 +0530

    message routes added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -15,8 +15,8 @@ const router = express.Router();
 
 router.use(arcjetProtection); // Apply Arcjet protection to all routes in this router
 
-router.post("/signup", signup);
 router.post("/login", login);
+router.post("/signup", signup);
 router.post("/logout", logout);
 router.put("/update-profile", protectRoute ,updateProfile);
 router.get("/check", protectRoute, (req,res) => res.status(200).json(req.user));
