d79a2b0e32d87022b7baf9899565d770f265e6ab
Harsh Bagwan <harshbagwan11@gmail.com>
2026-08-05 18:56:53 +0530

    signup implemented on the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,29 @@
-import React from "react";
-import { Route, Routes } from "react-router";
+import React, { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import { useAuthStore } from "./store/useAuthStore";
-
+import PageLoader from "./components/PageLoader";
+import { Toaster } from "react-hot-toast";
 function App() {
   // const [user, setUser] = React.useState(null);
   // const [myName, setMyName] = useState("Diddy Patel");
   //if you want to have a state that is global we always need to create it in the parent component and they share with the child component through props. In this case, App.jsx is the parent component and ChatPage.jsx is the child component. So we create a state in App.jsx and share it with ChatPage.jsx through props.
 
-  const { authUser, isLoggedIn, login } = useAuthStore();
-  console.log("authUser", authUser);
-  console.log("isloggedIn", isLoggedIn);
+  // const { authUser, isLoggedIn, login } = useAuthStore();
+  // console.log("authUser", authUser);
+  // console.log("isloggedIn", isLoggedIn);
+
+  const {checkAuth, isCheckingAuth, authUser} = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({authUser});
+
+  if (isCheckingAuth) return <PageLoader />;
+
   return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
       {/* DECORATORS - GRID BG & GLOW SHAPES */}
@@ -20,14 +31,16 @@ function App() {
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
 
-      <button onClick={login} className="z-10">login</button>
+      {/* <button onClick={login} className="z-10">login</button> */}
       <Routes>
         {/* <Route path="/" element={<ChatPage myName={myName} />} />
-      <Route path="/login" element={<LoginPage myName={myName}/>} /> */}
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage myName={myName}/>} /> */}
+        <Route path="/" element={authUser ? <ChatPage /> : <Navigate to={"/login"} />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"}/>} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={"/"}/>} />
       </Routes>
+
+      <Toaster/>
     </div>
   );
 }
