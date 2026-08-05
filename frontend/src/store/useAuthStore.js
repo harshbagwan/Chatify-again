b0da6a0799df888a2d407ecef336e9537f2e012b
Harsh Bagwan <harshbagwan11@gmail.com>
2026-08-05 19:50:58 +0530

    login & logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,17 +3,10 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
-
-  // authUser: { name: "diddy", _id:123,age:25},
-  // isLoggedIn:false,
-
-  // login: ()=>{
-  //     console.log("We just logged in");
-  //     set({isLoggedIn:true});
-  // },
   authUser: null,
   isCheckingAuth: true,
-  isSigningUp:false,
+  isSigningUp: false,
+  isLoggingIn: false,
 
   checkAuth: async () => {
     try {
@@ -27,21 +20,42 @@ export const useAuthStore = create((set) => ({
       set({ isCheckingAuth: false });
     }
   },
-
-  signup: async (data) => {  // data = {fullName,email,password} get the data from the form and send it to the backend
-    set({isSigningUp:true});
+  signup: async (data) => {
+    // data = {fullName,email,password} get the data from the form and send it to the backend
+    set({ isSigningUp: true });
     try {
       const res = await axiosInstance.post("/auth/signup", data); // we are sending the data to the backend using axiosInstance which is configured to send the request to the backend server. The backend server will handle the request and send the response back to the frontend. The response will contain the user data which we will set in the authUser state.
       set({ authUser: res.data }); // here res.data is the user data which we get from the backend after successful signup. We set this data in the authUser state so that we can use it in the frontend to show the user data and also to check if the user is logged in or not.
-    //   get().connectSocket();
-    toast.success("Account created successfully!"); // show a success toast message to the user after successful signup.
+      //   get().connectSocket();
+      toast.success("Account created successfully!"); // show a success toast message to the user after successful signup.
     } catch (error) {
-        // toast.error(error.response?.data?.message || "Something went wrong!"); // show an error toast message to the user if there is any error in the signup process. We are checking if the error response has a message property and if it does, we show that message to the user. If it doesn't, we show a generic error message.
-       toast.error(error.response.data.message);
-
+      // toast.error(error.response?.data?.message || "Something went wrong!"); // show an error toast message to the user if there is any error in the signup process. We are checking if the error response has a message property and if it does, we show that message to the user. If it doesn't, we show a generic error message.
+      toast.error(error.response.data.message);
     } finally {
-      set({isSigningUp:false});
+      set({ isSigningUp: false });
     }
-}
+  },
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
 
+      toast.success("Logged in successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error("Error logging out");
+      console.log("Logout error:", error);
+    }
+  },
 }));
