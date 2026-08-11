67ed951c34ab2973cec89952c5c76b59f4923118
Harsh Bagwan <harshbagwan11@gmail.com>
2026-08-11 14:06:49 +0530

    sidebar completed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,15 +4,6 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 const router = express.Router();
 
-// router.get("/test", arcjetProtection, (req,res)=>{
-//     res.status(200).json({message:"Test route"});
-// });
-// router.post("/signup",arcjetProtection, signup);
-// router.post("/login",arcjetProtection, login);
-// router.post("/logout",arcjetProtection, logout);
-// router.put("/update-profile",arcjetProtection, protectRoute ,updateProfile);
-// router.get("/check",arcjetProtection, protectRoute, (req,res) => res.status(200).json(req.user));
-
 router.use(arcjetProtection); // Apply Arcjet protection to all routes in this router
 
 router.post("/signup", signup);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -57,4 +57,19 @@ export const useAuthStore = create((set) => ({
       console.log("Logout error:", error);
     }
   },
+  updateProfile: async (data) => {
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } 
+    //  catch (error) {
+      //   console.log("Error in update profile:", error);
+      //   toast.error(error.response.data.message);
+      // }
+      catch (error) {
+      console.log("Error in update profile:", error);
+      toast.error(error.response?.data?.message || "Failed to update profile");
+    }
+  },
 }));
