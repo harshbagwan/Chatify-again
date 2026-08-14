938bd4be127cc3fabae9eb231258c010c7ffbfa3
Harsh Bagwan <harshbagwan11@gmail.com>
2026-08-14 19:27:35 +0530

    fix(backend): set sameSite lax for single-domain Render deployment

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -12,7 +12,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // milli seconds
     httpOnly: true, // prevent XSS attacks:cross-site scripting,, javascript code cannot access the cookie 
-    sameSite: ENV.NODE_ENV === "development" ? "lax" : "none",
+    sameSite: "lax",
     secure: ENV.NODE_ENV === "development" ? false : true,
   });
   return token;
